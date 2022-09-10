53632124f4cdc9b46692be98deb3cf1037999aac
Efim <evim_l@bk.ru>
2022-09-10 16:45:59 +0300

    add third (negative) test for set

--- test_all.py
+++ test_all.py
@@ -7,3 +7,18 @@ def test_set_add():
     initial_set = {"a", "b"}
     initial_set.add("c")
     assert initial_set == {"a", "b", "c"}
+
+
+def test_set_remove_twice():
+    initial_set = {"a", "b"}
+    initial_set.add("c")
+    
+    try:
+        initial_set.remove("c")
+        initial_set.remove("c")
+    except KeyError:
+        print('element already removed')
+
+    assert initial_set == {"a", "b"}
+
+
