920406e5d7dc44d38a906177c70791a5efbb5c4c
Efim <evim_l@bk.ru>
2022-09-10 17:03:17 +0300

    add first test for float

--- test_all.py
+++ test_all.py
@@ -1,3 +1,6 @@
+import pytest
+
+
 def test_set_creation():
     created_set = {"a", "b", "c"}
     assert created_set == {"a", "b", "c"}
@@ -20,5 +23,12 @@ def test_set_remove_twice():
         print('element already removed')
 
     assert initial_set == {"a", "b"}
+
+
+@pytest.mark.parametrize("test_input,expected", [("2", 2.0), (5, 5.0), (20.0, 20.0)])
+def test_float_convert(test_input, expected):
+    converted_to_float = float(test_input)
+    assert isinstance(converted_to_float, float) and converted_to_float == expected
+
 
 
