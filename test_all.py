fd02dd522577ebef02a7e26bc6bd4e92e53f9cdc
Efim <evim_l@bk.ru>
2022-09-10 16:32:53 +0300

    add second test for set

--- test_all.py
+++ test_all.py
@@ -1,3 +1,9 @@
 def test_set_creation():
     created_set = {"a", "b", "c"}
     assert created_set == {"a", "b", "c"}
+
+
+def test_set_add():
+    initial_set = {"a", "b"}
+    initial_set.add("c")
+    assert initial_set == {"a", "b", "c"}
